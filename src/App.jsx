791cfcaedeb12cc341fd9f9e5942421cbbd3c7b8
Archit <archit499kul@gmail.com>
2023-09-20 17:22:13 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Grid } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <>
+    <h1>GameHub</h1>
+    
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import App from "./App.jsx";
+import "./index.css";
+// 484e9b2b843a4d8a8beb72f0dbc4f3b8
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
