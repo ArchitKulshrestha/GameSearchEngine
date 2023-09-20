9f344562c1a107820fe6d8e5f808ac4fc69a333b
Archit <archit499kul@gmail.com>
2023-09-20 18:14:33 +0530

    ColorMode Toggle

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,19 @@
-import { HStack, Image } from "@chakra-ui/react"
-import logo from "../assets/logo.webp"
+import { HStack, Image } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack>
-        <Image src={logo} alt="logo" boxSize="70px"/>
+    <HStack
+      justifyContent={"space-between"}
+      paddingTop={"0"}
+      paddingBottom={"0"}
+      paddingLeft={"20px"}
+      paddingRight={"20px"}>
+      <Image src={logo} alt="logo" boxSize="70px" />
+      <ColorModeSwitch />
     </HStack>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
